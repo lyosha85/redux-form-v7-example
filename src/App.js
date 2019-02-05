647ceed178d5ee35650595ed93daaae15c25cbc6
Alex <lyosha85@gmail.com>
2019-02-05 15:43:25 +0900

    Add redux store

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DemoForm from './DemoForm';
+import store from './store';
+import { Provider } from 'react-redux';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<DemoForm />
-			</div>
+			<Provider store={store}>
+				<div className="App">
+					<DemoForm />
+				</div>
+			</Provider>
 		);
 	}
 }
