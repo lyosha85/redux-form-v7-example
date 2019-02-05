16b012745e562ff22325ccaf3d82d9aeb8d81f61
Alex <lyosha85@gmail.com>
2019-02-05 17:33:22 +0900

    Handle submit with a fake submit

--- src/DemoForm.js
+++ src/DemoForm.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import showResults from './showResults';
 
-let DemoForm = ({ handleSubmit }) => {
+let DemoForm = ({ handleSubmit, submitting }) => {
 	return (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit(showResults)}>
 			<label htmlFor="cut_off_time">Cut off time</label>
 			<Field name="cut_off_time" component="input" />
-			<button type="Submit">Submit</button>
+			<button type="Submit" disabled={submitting}>
+				Submit
+			</button>
 		</form>
 	);
 };
