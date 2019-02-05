f03a204888c938a0746cc2340a4230128cc90314
Alex <lyosha85@gmail.com>
2019-02-05 17:44:34 +0900

    Add validation using the redux-form validate method

--- src/DemoForm.js
+++ src/DemoForm.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import showResults from './showResults';
 
+const validate = values => {
+	const errors = {};
+	if (!values.cut_off_time) {
+		errors.cut_off_time = 'Required';
+	}
+	return errors; // has to be the exact same shape as values object
+};
+
 let DemoForm = ({ handleSubmit, submitting }) => {
 	return (
 		<form onSubmit={handleSubmit(showResults)}>
@@ -15,7 +23,8 @@ let DemoForm = ({ handleSubmit, submitting }) => {
 };
 
 DemoForm = reduxForm({
-	form: 'demo'
+	form: 'demo',
+	validate
 })(DemoForm);
 
 export default DemoForm;
