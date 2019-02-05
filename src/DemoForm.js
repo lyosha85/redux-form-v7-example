4aaa2fbdbf89779c943c87fecd35ab647ac3bcf3
Alex <lyosha85@gmail.com>
2019-02-05 15:23:30 +0900

    decorate form with reduxForm features

--- src/DemoForm.js
+++ src/DemoForm.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { reduxForm } from 'redux-form';
 
-const DemoForm = () => {
+let DemoForm = () => {
 	return <div>Test</div>;
 };
+
+DemoForm = reduxForm({})(DemoForm);
 
 export default DemoForm;
