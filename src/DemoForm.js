b63fad9612be1c18ddf281f415db64ddfa23ab48
Alex <lyosha85@gmail.com>
2019-02-07 11:29:19 +0900

    Extract to renderer and add select

--- src/DemoForm.js
+++ src/DemoForm.js
@@ -16,20 +16,24 @@ const validate = values => {
 	return errors; // has to be the exact same shape as values object
 };
 
-const renderInput = ({ input, meta, label }) => {
+const createRenderer = render => ({ input, meta, label }) => {
 	return (
 		<div>
 			{meta.error && meta.touched ? <span> {meta.error} </span> : ''}
 			<label>{label}</label>
-			<input {...input} />
+			{render(input, label)}
 		</div>
 	);
 };
 
+const RenderInput = createRenderer((input, label) => <input {...input} placeholder={label} />);
+const RenderSelect = createRenderer((input, label) => <select {...input} placeholder={label} />);
+
 let DemoForm = ({ handleSubmit, submitting }) => {
 	return (
 		<form onSubmit={handleSubmit(showResults)}>
-			<Field label="Cut off time" name="cut_off_time" component={renderInput} />
+			<Field label="Cut off time" name="cut_off_time" component={RenderInput} />
+			<Field label="Open?" name="open_or_closed" component={RenderSelect} />
 			<button type="Submit" disabled={submitting}>
 				Submit
 			</button>
